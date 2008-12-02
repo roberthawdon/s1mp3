ca2c68eaee0214166d8cfd47ff20cbc4591600c6
purdea_andrei <purdea_andrei@50f6bb0f-713a-0410-b1b0-c2d32e788cf8>
2008-12-02 09:10:05 +0000

    fixing "invalid file type" error from link-z80

--- trunk/common/giveio.bin/make.inc.mk
+++ trunk/common/giveio.bin/make.inc.mk
@@ -3,11 +3,11 @@ GNAME=giveio
 GPREF=$(GFPREF)$(GNAME)
 
 
-$(GPREF).obj: $(GPREF).asm inc/s1reg.inc inc/s1sys.inc
-	cd $(GFPREF); as-z80 -o $(GNAME).obj $(GNAME).asm
+$(GPREF).o: $(GPREF).asm inc/s1reg.inc inc/s1sys.inc
+	cd $(GFPREF); as-z80 -o $(GNAME).o $(GNAME).asm
 
-$(GPREF).ihx: $(GPREF).obj
-	link-z80 -n -- -i $(GPREF).ihx $(GPREF).obj
+$(GPREF).ihx: $(GPREF).o
+	sdcc -mz80 --no-std-crt0 $(GPREF).o -o $(GPREF).ihx
 
 $(GPREF).bin: $(GPREF).ihx $(GFPREF)utils/ihx2bin
 	$(GFPREF)utils/ihx2bin $(GPREF).bin $(GPREF).ihx
@@ -16,6 +16,6 @@ $(GPREF).h: $(GPREF).bin $(GFPREF)utils/bin2c$(EXT)
 	$(GFPREF)utils/bin2c$(EXT) $(GPREF).bin giveio > $(GPREF).h
 
 clean_giveio_bin: clean_utils
-	$(DEL) $(GPREF).obj $(GPREF).ihx $(GPREF).bin $(GPREF).h
+	$(DEL) $(GPREF).o $(GPREF).ihx $(GPREF).bin $(GPREF).h
 
 include $(GFPREF)utils/make.inc.mk
